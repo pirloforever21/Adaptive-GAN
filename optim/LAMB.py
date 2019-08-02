c524516522b08dfcb015219590f9d79751964e8e
Zhenhuan(Neyo) Yang <zhenhuan.yang@hotmail.com>
2019-08-02 14:39:03 +0800

    Update LAMB.py

--- optim/LAMB.py
+++ optim/LAMB.py
@@ -31,7 +31,7 @@ class LAMB(Optimizer):
     """
 
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-6,
-                 weight_decay=0.1):
+                 weight_decay=0):
         if not 0.0 <= lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if not 0.0 <= eps:
